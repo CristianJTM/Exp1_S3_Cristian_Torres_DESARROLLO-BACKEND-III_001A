b2680f7d5c43d0c5ac705ed683eac95bb0934214
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 19:21:54 -0300

    feat: initialize BffMobileApplication with Maven wrapper and basic configuration

---
+++ bff-mobile/src/test/java/com/bancoxyz/bff/mobile/BffMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoxyz.bff.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
