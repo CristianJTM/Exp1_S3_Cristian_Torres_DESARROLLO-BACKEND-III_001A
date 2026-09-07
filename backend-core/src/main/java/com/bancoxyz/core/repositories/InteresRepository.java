5179e8ce848c01775d65189f76d9834acce61a50
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 18:30:05 -0300

    refactor: remove InteresController, InteresDTO, Interes, InteresRepository, and InteresService classes

--- backend-core/src/main/java/com/bancoxyz/core/repositories/InteresRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bancoxyz.core.repositories;
-
-import com.bancoxyz.core.model.Interes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InteresRepository extends JpaRepository<Interes, Long> {
-
-}
