9b6fdb004e5193b21571474536dd2985d99e0c18
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 18:40:02 -0300

    feat: initialize BFF web application with Maven setup and basic structure

---
+++ bff-web/src/test/java/com/bancoxyz/bff/web/BffWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoxyz.bff.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
