088748add681c9d18db6275c02b5c1469300fc95
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 07:38:37 -0300

    refactor: update package structure for BackendCoreApplication

--- backend-core/src/main/java/com/bancoxyz/core/BackendCoreApplication.java
+++ backend-core/src/main/java/com/bancoxyz/core/BackendCoreApplication.java
@@ -1,4 +1,4 @@
-package com.bancoxyz.backend_core;
+package com.bancoxyz.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
