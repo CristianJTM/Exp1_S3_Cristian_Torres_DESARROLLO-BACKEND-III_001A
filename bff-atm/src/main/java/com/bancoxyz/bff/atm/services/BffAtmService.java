34be3e5fac9d2b8470539ae8ae5868367a2626fa
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 22:52:32 -0300

    feat: add BffAtmService for account balance inquiry and withdrawal operations

---
+++ bff-atm/src/main/java/com/bancoxyz/bff/atm/services/BffAtmService.java
@@ -0,0 +1,50 @@
+package com.bancoxyz.bff.atm.services;
+
+import com.bancoxyz.bff.atm.clients.BackendCoreClient;
+import com.bancoxyz.bff.atm.dtos.CuentaAtmDTO;
+import com.bancoxyz.bff.atm.dtos.RetiroAtmRequestDTO;
+import com.bancoxyz.bff.atm.dtos.RetiroAtmResponseDTO;
+import com.bancoxyz.bff.atm.dtos.core.CuentaCoreDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BffAtmService {
+
+    private final BackendCoreClient backendCoreClient;
+
+    public CuentaAtmDTO consultarSaldo(Long cuentaId) {
+
+        CuentaCoreDTO cuenta =
+                backendCoreClient.obtenerCuenta(cuentaId);
+
+        CuentaAtmDTO respuesta = new CuentaAtmDTO();
+
+        respuesta.setCuentaId(cuenta.getCuentaId());
+        respuesta.setSaldo(cuenta.getSaldo());
+
+        return respuesta;
+    }
+
+    public RetiroAtmResponseDTO realizarRetiro(
+            Long cuentaId,
+            RetiroAtmRequestDTO retiro) {
+
+        CuentaCoreDTO cuenta =
+                backendCoreClient.realizarRetiro(
+                        cuentaId,
+                        retiro
+                );
+
+        RetiroAtmResponseDTO respuesta =
+                new RetiroAtmResponseDTO();
+
+        respuesta.setCuentaId(cuenta.getCuentaId());
+        respuesta.setMontoRetirado(retiro.getMonto());
+        respuesta.setSaldo(cuenta.getSaldo());
+        respuesta.setEstado("APROBADO");
+
+        return respuesta;
+    }
+}
