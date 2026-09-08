8336e6555323fd81e4f2d26cc16989dba675f1ca
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 22:25:05 -0300

    feat: enable Feign clients in BffMobileApplication for service communication

--- bff-mobile/src/main/java/com/bancoxyz/bff/mobile/BffMobileApplication.java
+++ bff-mobile/src/main/java/com/bancoxyz/bff/mobile/BffMobileApplication.java
@@ -2,12 +2,13 @@ package com.bancoxyz.bff.mobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BffMobileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BffMobileApplication.class, args);
 	}
-
 }
