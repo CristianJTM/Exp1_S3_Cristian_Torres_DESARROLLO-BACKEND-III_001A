77c722699a4b3379ac3025f13b805c41d87f3c53
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 22:52:40 -0300

    feat: enable Feign clients in BffAtmApplication for service communication

--- bff-atm/src/main/java/com/bancoxyz/bff/atm/BffAtmApplication.java
+++ bff-atm/src/main/java/com/bancoxyz/bff/atm/BffAtmApplication.java
@@ -2,12 +2,13 @@ package com.bancoxyz.bff.atm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BffAtmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BffAtmApplication.class, args);
 	}
-
 }
