d5747c219f595220b411473a0851ad0f5eee3733
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 22:24:11 -0300

    feat: add custom query to TransaccionRepository for retrieving the latest transaction ID

--- backend-core/src/main/java/com/bancoxyz/core/repositories/TransaccionRepository.java
+++ backend-core/src/main/java/com/bancoxyz/core/repositories/TransaccionRepository.java
@@ -2,10 +2,14 @@ package com.bancoxyz.core.repositories;
 
 import com.bancoxyz.core.model.Transaccion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TransaccionRepository extends JpaRepository<Transaccion, Long> {
 
     List<Transaccion> findByCuentaId(Long cuentaId);
+
+    @Query("SELECT MAX(t.id) FROM Transaccion t")
+    Long obtenerUltimoId();
 }
