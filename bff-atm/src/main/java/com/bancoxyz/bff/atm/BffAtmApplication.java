a551d0354faa8b3b6826a60db1eb78d22b35f9a9
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 22:23:43 -0300

    feat: initialize BffAtmApplication with Maven wrapper and basic configuration

---
+++ bff-atm/src/main/java/com/bancoxyz/bff/atm/BffAtmApplication.java
@@ -0,0 +1,13 @@
+package com.bancoxyz.bff.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAtmApplication.class, args);
+	}
+
+}
---
+++ bff-atm/src/test/java/com/bancoxyz/bff/atm/BffAtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoxyz.bff.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
