e7d23e4b66f9a6035aaa07394397728096fb4b1b
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-09-07 22:23:51 -0300

    feat: add endpoint for performing withdrawals in CuentaController

--- backend-core/src/main/java/com/bancoxyz/core/controllers/CuentaController.java
+++ backend-core/src/main/java/com/bancoxyz/core/controllers/CuentaController.java
@@ -1,12 +1,10 @@
 package com.bancoxyz.core.controllers;
 
 import com.bancoxyz.core.dtos.CuentaDTO;
+import com.bancoxyz.core.dtos.RetiroDTO;
 import com.bancoxyz.core.services.CuentaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/cuentas")
@@ -19,5 +17,16 @@ public class CuentaController {
     public CuentaDTO buscarPorId(@PathVariable Long cuentaId) {
 
         return cuentaService.buscarPorId(cuentaId);
+    }
+
+    @PostMapping("/{cuentaId}/retiros")
+    public CuentaDTO realizarRetiro(
+            @PathVariable Long cuentaId,
+            @RequestBody RetiroDTO retiro) {
+
+        return cuentaService.realizarRetiro(
+                cuentaId,
+                retiro.getMonto()
+        );
     }
 }
